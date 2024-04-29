0694ab3230a3ac90c91b346a95c774b233cd199a
ritu-02 <raniritu2811@gmail.com>
2024-04-29 19:43:08 +0530

    update commit

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -4,8 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class WordCounter extends JFrame implements ActionListener {
-    JLabel label1 , label2, label3;
-    JButton btn1, btn2,btn3;
+    JLabel label1 , label2;
+    JButton btn1, btn2;
     JTextArea textArea;
     JScrollPane scrollPane;
 
@@ -36,11 +36,6 @@ public class WordCounter extends JFrame implements ActionListener {
         label2.setFont(new Font("Italic",Font.BOLD,14));
         label2.setForeground(Color.blue);
 
-        label3 = new JLabel("Line Count : 0");
-        label3.setBounds(90,500,200,40);
-        label3.setFont(new Font("Italic",Font.BOLD,14));
-        label3.setForeground(Color.blue);
-
         btn1 = new JButton("Word Counter");
         btn1.setBounds(350,400,200,40);
         btn1.setFont(new Font("Italic",Font.BOLD,14));
@@ -51,23 +46,16 @@ public class WordCounter extends JFrame implements ActionListener {
         btn2.setFont(new Font("Italic",Font.BOLD,14));
         btn2.setForeground(Color.CYAN);
 
-        btn3 = new JButton("Line Counter");
-        btn3.setBounds(350,500,200,40);
-        btn3.setFont(new Font("Italic",Font.BOLD,14));
-        btn3.setForeground(Color.CYAN);
 
         this.add(scrollPane);
         // this.add(textArea);
         this.add(label1);
         this.add(label2);
-        this.add(label3);
         this.add(btn1);
         this.add(btn2);
-        this.add(btn3);
 
         btn1.addActionListener(this);
         btn2.addActionListener(this);
-        btn3.addActionListener(this);
 
         this.setVisible(true);
     }
@@ -76,17 +64,13 @@ public class WordCounter extends JFrame implements ActionListener {
          String text = textArea.getText();
          String[] words = text.split("\\s+");
          // To split by one or more whitespace
-         String[] lines = text.split("\n");
+         String[] lines = text.split(".");
 
          if (e.getSource()==btn1){
              label1.setText("Words Count : "+words.length);
-         } else if (e.getSource()==btn2) {
+         } else  {
              // Remove whitespaces and count characters
              label2.setText("Characters Count : "+text.replaceAll("\\s","").length());
-         }else {
-             // Remove trailing empty lines
-             int count = text.isEmpty()?0:text.split("\n").length;
-             label3.setText("Lines Count : "+count);
          }
 
     }
